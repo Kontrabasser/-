e88facc88f4c7ff24f03551700042572a9275859
Oles Labur <MY_NAME@example.com>
2023-10-02 12:28:29 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Askold
+{
+    static void Main()
+    {
+        Console.WriteLine("введіть число");
+        string input = Console.ReadLine();
+        string[] inputNumbers = input.Split(' ');
+
+        int negativeCount = 0;
+int i = 0;
+
+       while (inputNumbers.Length > i)
+        {
+            var inputNumber = inputNumbers [i] ;
+            i ++;
+            if (int.TryParse(inputNumber, out int parsedNumber) && parsedNumber < 0)
+            {
+                negativeCount++;
+            }
+        }
+
+        Console.WriteLine("Кількість негативних чисел: " + negativeCount);
+    }
+}
